3136cd219b477cda0f54bb4b7a979f34903eb868
leonelgpr33 <leonel.perez@mrci.com.mx>
2015-01-17 14:24:07 -0600

    Prueba de Commit

---
+++ Practica1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Practica1
+//
+//  Created by LI Leonel G. Pérez Ramos on 17/01/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Practica1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Practica1
+//
+//  Created by LI Leonel G. Pérez Ramos on 17/01/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
